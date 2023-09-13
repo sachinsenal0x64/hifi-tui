e3bded2eae9da32b9d9dd90c248b5f08d5c3a134
sachin senal <sachinsenal0x64@gmail.com>
2023-09-14 06:23:39 +0800

    fixes

--- tidal_tui.py
+++ tidal_tui.py
@@ -2,22 +2,13 @@ from textual.app import App, ComposeResult
 from textual.widgets import Static
 
 
-class VerticalLayoutExample(App):
-    CSS_PATH = "tidal_tui.css"
-
-    def compose(self) -> ComposeResult:
-        yield Static(classes="box")
-
-
-class SearchBox(VerticalLayoutExample):
-    def searchbox(self, App) -> ComposeResult:
-        super().__init__(App)
-        yield Static(classes="s_box")
+class SearchBox(App):
+    def searchbox(self) -> ComposeResult:
+        yield Static(classes="box", id="box")
+        yield Static(classes="box2", id="box2")
 
 
 if __name__ == "__main__":
     app = SearchBox()
-
-    app.compose()
 
     app.run()
