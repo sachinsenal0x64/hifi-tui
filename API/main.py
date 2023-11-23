d201467b2317ce393779a38f8f0806f6899f070d
sachinsenal0x64 <sachinsenal0x64@gmail.com>
2023-11-22 23:20:12 -0800

    Update main.py

--- API/main.py
+++ API/main.py
@@ -94,6 +94,35 @@ async def search_track(q: str | None):
         return search_data.json()
 
 
+@app.api_route("/cover/", methods=["GET"])
+async def search_cover(id: str | None = None, q: str | None = None):
+    search_url = f"https://api.tidal.com/v1/search/tracks?countryCode=US&query={q}"
+    header = {"authorization": f"Bearer {tidal_token}"}
+    async with httpx.AsyncClient() as clinet:
+        cover_data = await clinet.get(url=search_url, headers=header)
+        tracks = cover_data.json()["items"][:10]
+
+        album_ids = []  # list to store album ids
+
+        for track in tracks:
+            album_cover = track["album"]["cover"].replace("-", "/")
+            album_name = track["album"]["title"]
+            album_ids.append(album_cover)
+            album_ids.append(album_name)
+
+        json_data = [
+            {
+                "name": album_ids[i + 1],
+                "cover1": f"https://resources.tidal.com/images/{album_ids[i]}/1280x1280.jpg",
+                "cover2": f"https://resources.tidal.com/images/{album_ids[i]}/80x80.jpg",
+            }
+            for i in range(0, len(album_ids), 2)
+        ]
+
+        # Create a list of dictionaries with "cover" and "name" keys
+        return json_data
+
+
 async def main():
     config = uvicorn.Config("main:app", port=5000)
     server = uvicorn.Server(config)
