43152e4e9325af9cb8055e783b54f8fcddfcfd6d
Your Name <you@example.com>
2024-01-28 21:00:21 +0100

    event loop

--- TUI/go.mod
+++ TUI/go.mod
@@ -4,12 +4,12 @@ go 1.21.6
 
 require (
 	github.com/gdamore/tcell v1.4.0
+	github.com/gdamore/tcell/v2 v2.6.1-0.20231203215052-2917c3801e73
 	github.com/rivo/tview v0.0.0-20240122063236-8526c9fe1b54
 )
 
 require (
 	github.com/gdamore/encoding v1.0.0 // indirect
-	github.com/gdamore/tcell/v2 v2.6.1-0.20231203215052-2917c3801e73 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-runewidth v0.0.14 // indirect
 	github.com/rivo/uniseg v0.4.3 // indirect
--- TUI/main.go
+++ TUI/main.go
@@ -1,20 +1,54 @@
 package main
 
 import (
+	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 )
 
+// song informations
+
+type Songs struct {
+	duration float64
+	title    string
+	artist   string
+	album    string
+}
+
 func main() {
 	app := tview.NewApplication()
+
+	// 2x2
 
 	flex := tview.NewFlex().
 		AddItem(tview.NewFlex().SetDirection(tview.FlexRow).
 			AddItem(tview.NewBox().SetBorder(true).SetTitle("Library"), 0, 1, false).
 			AddItem(tview.NewBox().SetBorder(true).SetTitle("Lyrics"), 0, 1, false), 0, 1, false).
+
+		// 2x2
+
 		AddItem(tview.NewFlex().SetDirection(tview.FlexRow).
 			AddItem(tview.NewBox().SetBorder(true).SetTitle("Songs"), 0, 10, false).
 			AddItem(tview.NewBox().SetBorder(true).SetTitle("Player"), 0, 1, false), 0, 3, false)
+
+	// Set up an event loop
+	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		// Check for the Escape key press
+		if event.Rune() == 'q' {
+			app.Stop()
+		}
+		switch event.Rune() {
+		case 'k':
+			app.Stop()
+		case 'j':
+			return nil
+		}
+		return event
+	})
+
+	// The application will run until app.Stop() is called (in response to the Escape key press)
+
 	if err := app.SetRoot(flex, true).Run(); err != nil {
 		panic(err)
 	}
+
 }
