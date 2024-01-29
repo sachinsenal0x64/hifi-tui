44371e35e05a86aa178560e31d594f057543ac25
Your Name <you@example.com>
2024-01-29 11:13:47 +0100

    layout

--- TUI/components/song.go
+++ TUI/components/song.go
@@ -4,18 +4,18 @@ import (
 	"github.com/rivo/tview"
 )
 
-type LibraryComponets struct {
+type SongComponets struct {
 	Container     *tview.Flex
 	BodyComponent *tview.TextArea
 }
 
-func CreateLibrary() *LibraryComponets {
+func CreateSong() *SongComponets {
 	ct := tview.NewFlex()
-	ct.SetDirection(tview.FlexRow).SetBorder(true).SetTitle("Library")
+	ct.SetDirection(tview.FlexRow).SetBorder(true).SetTitle("Song")
 
 	// Add library component on its container
 
-	return &LibraryComponets{
+	return &SongComponets{
 		Container: ct,
 	}
 }
