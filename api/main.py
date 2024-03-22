d2a4fd8b5181e4eb7f09c8a4cbfb4c6391b5390e
Your Name <you@example.com>
2024-03-22 09:04:42 +0100

    🧩 /doc endpoint added

--- api/main.py
+++ api/main.py
@@ -150,7 +150,7 @@ async def favicon():
     return FileResponse(f"{dirname}/favicon.ico")
 
 
-@app.api_route("/", methods=["GET"])
+@app.api_route("/", methods=["GET"], include_in_schema=False)
 async def index():
     return {"HIFI-API": "v1", "REPO": "https://github.com/sachinsenal0x64/Hifi-Tui"}
 
